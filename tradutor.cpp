7a34d5bbf22b128b3c316fb01622846af2b9dabd
loenz-lukas <lukaslorenzdeandrade@gmail.com>
2019-06-29 12:37:04 -0300

    preprocessing done

--- tradutor.cpp
+++ tradutor.cpp
@@ -4,6 +4,7 @@
 //#include <string.h>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <iostream>
 #include <list>
 
@@ -18,6 +19,7 @@ void preprocessing(string in_file, string out_file){
   string linha, palavra, rotulo, operacao, operando1, operando2, adicionado,operando3;
   char letra, letra_anterior;
   int i, j,letrint, parametro, tam_rot, mudarot=0, mudaop=0, mudaop1=0, mudaop2=0, ignoralinha=0, mudaadic=0, contadorlinha=1;
+  int temp = 0;
   bool ok = false;
   list<diretiva> tab_preproc;
   diretiva elemento_preproc;
@@ -45,24 +47,22 @@ void preprocessing(string in_file, string out_file){
             if(letra != ' ' && letra != '\t'  && letra != '+' && letra != ':'){ //verifica se o char faz parte de alguma palavra//
                 if (letra > 64 && letra < 91) letra = letra + 32;
                 palavra += letra;
-                if((i+1)==linha.length() || linha[i+1] == ' ' || linha[i+1] == ':' || linha[i+1] == '\t'){
+                if(((i+1)==linha.length() || linha[i+1] == ' ' || linha[i+1] == ':' || linha[i+1] == '\t')){
                     ok=true;
                     letra = linha[i+1];
                 }
             }
             if(ok){
-                cout<< palavra << endl;
-                if ((letra == ' ' || letra == '\n' || letra == '\t' || letra == ':' || letra == '+')){ //verifica se eh termino de palavra//
+                //cout<< palavra << endl;
+                if ((letra == ' ' || letra == '\n' || (i+1)==linha.length() || letra == '\t' || letra == ':' || letra == '+')){ //verifica se eh termino de palavra//
                   if (letra == ':'){ //eh rotulo?//
                       mudarot = mudarot + 1;
                       rotulo = palavra;
                       cout<<"Rotulo: "<< rotulo << endl;
-                  }
-                  else if (mudaadic == 1) {
+                  }else if (mudaadic == 1) {
                       adicionado =  palavra;
                       cout<< "Adicionado:\t" << adicionado << endl;
-                  }
-                  else {
+                  }else{
                       if (letra == '+') {
                         mudaadic = 1;
                       }
@@ -86,7 +86,7 @@ void preprocessing(string in_file, string out_file){
                       }
                       parametro = parametro + 1;
                     }
-                    i=0;
+                    //i=0;
                 }
 
                 if ((i+1)==linha.length() && mudaop == 1){ //se a linha terminou//
@@ -94,7 +94,8 @@ void preprocessing(string in_file, string out_file){
                     if (!operacao.compare("equ")){ // se for equ, armazena nome do rotulo e valor
                         cout << "Rotulo:\t" << rotulo << endl;
                         elemento_preproc.nome = rotulo;
-                        elemento_preproc.valor = stoi(operando1); //corrigir o inteiro dado pela tabela ascii
+                        stringstream geek(operando1); //corrigir o inteiro dado pela tabela ascii
+                        geek >> elemento_preproc.valor;
                         cout << "Elemento_nome:\t" << elemento_preproc.nome << endl;
                         cout << "valor:\t" << elemento_preproc.valor << endl;
                         tab_preproc.push_back(elemento_preproc);
@@ -115,21 +116,25 @@ void preprocessing(string in_file, string out_file){
                         }
                     }
                     else{
-                          cout<< "mudarot: \t"<< mudarot << endl;
-                          if (mudarot > 0) fs2 << rotulo + ":" + "\t";
-                          fs2 << operacao;
-                          if (mudaop1 == 1) fs2 << "\t" + operando1;
-                          if (mudaop2 == 1) fs2 << "\t" + operando2;
-                          if(parametro > 3) {
-                            for(j=4; j < parametro; j++) fs2 << "\t" + operando3[j-4];
-                          }
-                          if (mudaadic == 1) fs2 << "+" + adicionado;
-                          fs2 << endl;
+                          if(ignoralinha == 0){
+                              cout<< "mudarot: \t"<< mudarot << endl;
+                              if (mudarot > 0) fs2 << rotulo + ":" + "\t";
+                              fs2 << operacao;
+                              if (mudaop1 == 1) fs2 << "\t" + operando1;
+                              if (mudaop2 == 1) fs2 << "\t" + operando2;
+                              if(parametro > 3) {
+                                for(j=4; j < parametro; j++) fs2 << "\t" + operando3[j-4];
+                              }
+                              if (mudaadic == 1) fs2 << "+" + adicionado;
+                              fs2 << endl;
+                         }
+                         cout<<"oi"<<endl;
+                         ignoralinha = 0;
                     }
                   }
                   contadorlinha++;
                   parametro = 1;
-                  printf(" \n");
+                  cout<<endl;
                   mudarot = 0; mudaop = 0; mudaop1 = 0; mudaop2 = 0; mudaadic = 0;
                 }
                 palavra.erase(palavra.begin(), palavra.end());
@@ -189,7 +194,23 @@ int main (int argc, char *argv[]){
             }
             cout<< token << endl;
           }
-          //continue
+          /*num_op = descobreinstrucao(operacao);
+          num_dir = descobrediretiva(operacao);
+          if (num_dir == 3) {
+            if (strcmp(operando1,"text") == 0) {
+              secao = 0;
+            }
+            else if (strcmp(operando1,"data") == 0) {
+              secao = 1;
+            }
+            else if (strcmp(operando1,"bss") == 0) {
+              secao = 2;
+            }
+          }
+          converteia32(num_op, num_dir, entrada, saida, rotulo, operacao, operando1, operando2, adicionado, mudaop, mudaadic, mudarot, secao);
+          parametro = 1;
+          mudarot = 0; mudaop = 0; mudaop1 = 0; mudaop2 = 0; mudaadic = 0;
+          cout << endl;*/
       }
       fs1.close();
       fs2.close();
